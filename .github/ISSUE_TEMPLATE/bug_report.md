f0d76f0f6ee7017e39f166ab7adb446596868ada
NachoEchevarria <ignacio.echevarria@datadoghq.com>
2026-02-09 16:17:24 +0100

    Add present date

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -10,7 +10,7 @@ assignees: ''
 Unless explicitly stated otherwise all files in this repository are licensed
 under the Apache License Version 2.0.
 This product includes software developed at Datadog (https://www.datadoghq.com/).
-Copyright 2025 Datadog, Inc.
+Copyright 2025-present Datadog, Inc.
 -->
 
 ## Describe the bug
--- generate-license-csv.py
+++ generate-license-csv.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2025-present Datadog, Inc.
+
 """
 Generate LICENSE-3rdparty.csv from libdatadog's LICENSE-3rdparty.yml
 
